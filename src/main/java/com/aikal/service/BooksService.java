c3f260e28fb8b689c556f7ec161eff9af4dd25f9
aikalalfrian <aikal.alfrian@mekari.com>
2022-08-10 01:16:45 +0700

    update comment

--- src/main/java/com/aikal/service/BooksService.java
+++ src/main/java/com/aikal/service/BooksService.java
@@ -8,30 +8,29 @@ import org.springframework.stereotype.Service;
 import com.aikal.model.Books;
 import com.aikal.repository.BooksRepository;
 
-//defining the business logic  
 @Service
 public class BooksService {
     @Autowired
     BooksRepository booksRepository;
 
-    // getting all books record by using the method findaAll() of CrudRepository
+    // get all book record from the database
     public List<Books> getAllBooks() {
         List<Books> books = new ArrayList<Books>();
         booksRepository.findAll().forEach(books1 -> books.add(books1));
         return books;
     }
 
-    // getting a specific record by using the method findById() of CrudRepository
+    // get a book record from the database by id
     public Books getBooksById(int id) {
         return booksRepository.findById(id).get();
     }
 
-    // saving a specific record by using the method save() of CrudRepository
+    // post a book record to the database
     public void saveOrUpdate(Books books) {
         booksRepository.save(books);
     }
 
-    // deleting a specific record by using the method deleteById() of CrudRepository
+    // delete a book record from the database by id
     public void delete(int id) {
         booksRepository.deleteById(id);
     }
